feab77aef15fe2a10301209c031e991efae9bd49
Miro <MiroPhoenix91@gmail.com>
2021-04-23 23:11:11 +0200

    video component

---
+++ src/components/TheVideo.vue
@@ -0,0 +1,13 @@
+<template>
+video
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <TheVideo></TheVideo>
+  </div>
+</template>
+
+<script>
+import TheVideo from '../components/TheVideo.vue'
+
+export default {
+  name: 'Home',
+  components: {TheVideo}
+}
+</script>
